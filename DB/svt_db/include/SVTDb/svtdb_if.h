d67732ad9062a8fd7f9acc577dd866df95fe1205
Yasser Corrales <yasser.corrales@gmail.com>
2025-03-02 05:46:06 +0100

    ycm - update database

--- DB/svt_db/include/SVTDb/svtdb_if.h
+++ DB/svt_db/include/SVTDb/svtdb_if.h
@@ -1,16 +1,21 @@
 #ifndef SVTDB_IF_H
 #define SVTDB_IF_H
 
+/*!
+ * @file svtdb_if.h
+ * @author Y. Corrales <ycmorales@bnl.gov>
+ * @date Mar 2024
+ * @brief Database interface for SVT test
+ */
+
 #include <string>
 #include <vector>
 
 namespace SvtDb_IF
 {
-  // v1 of data model
-  /**************************************************************
-    Structure type definitions
-  **************************************************************/
-
+  //!
+  //! Structure type definitions
+  //!
   using dbVersion = struct dbVersion_s
   {
     int id;
@@ -19,10 +24,30 @@ namespace SvtDb_IF
     std::string description;
   };
 
-  /**************************************************************
-    Function declarations
-  **************************************************************/
+  using dbWaferRecords = struct dbWaferRecords
+  {
+    int id;
+    std::string_view serialNumber;
+    int batchNumber = -1;
+    std::string engineeringRun;
+    std::string foundry;
+    std::string technology;
+    std::string thinningDate;
+    std::string dicingdate;
+    std::string waferType;
+  };
+
+  //!
+  //! Function declarations
+  //!
+  std::vector<std::string> getAllEnumValues(std::string enum_name);
+  bool addEnumValue(std::string type_name, std::string value);
+
   int getAllVersions(std::vector<dbVersion> &versions);
+  int getAllWafers(std::vector<dbWaferRecords> &wafers);
+
+  bool insertWaferRecords(const dbWaferRecords &waferRecords);
+
 }  // namespace SvtDb_IF
 
 #endif
