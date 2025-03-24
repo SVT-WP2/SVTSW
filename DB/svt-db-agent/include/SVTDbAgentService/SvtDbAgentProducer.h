1ac95f854f300cbaf10844102d33d95640d92ba3
Yasser Corrales <yasser.corrales@gmail.com>
2025-03-24 16:26:19 +0100

    ycm - change Epic prefix to Svt

---
+++ DB/svt-db-agent/include/SVTDbAgentService/SvtDbAgentProducer.h
@@ -0,0 +1,50 @@
+#ifndef SVT_DB_AGENT_PRODUCER_H
+#define SVT_DB_AGENT_PRODUCER_H
+
+/*!
+ * @file SvtDbAgentProducer.h
+ * @author Y. Corrales <ycorrale@cern.ch>
+ * @date Mar-2025
+ * @brief Db-agent service producer
+ */
+
+#include "SVTUtilities/SvtLogger.h"
+
+#include <librdkafka/rdkafkacpp.h>
+
+#include <memory>
+#include <string>
+
+class SvtDbAgentMessage;
+
+class SvtDbAgentProducer
+{
+ public:
+  SvtDbAgentProducer(const std::string &broker);
+  ~SvtDbAgentProducer()
+  {
+    while (m_producer->outq_len() > 0)
+    {
+      logger.logInfo("Waiting for " + std::to_string(m_producer->outq_len()));
+      m_producer->poll(1000);
+    }
+  };
+
+  bool CreateProducer();
+
+  bool Push(SvtDbAgentMessage &message);
+
+ private:
+  SvtLogger &logger = Singleton<SvtLogger>::instance();
+
+  std::shared_ptr<RdKafka::Producer> m_producer;
+  std::shared_ptr<RdKafka::Topic> m_topic;
+  int m_partition = 0;
+
+  std::string m_broker;
+  std::string m_errStr;
+  std::string m_debug;
+  bool m_dumpConfig = false;
+};
+
+#endif  // !SVTDB_AGENT_H
---
+++ DB/svt-db-agent/src/SVTUtilities/SvtUtilities.cpp
@@ -0,0 +1,8 @@
+/*!
+ * @file SvtUtilities.cpp
+ * @author Y. Corrales <ycorrale@cern.ch>
+ * @date Mar-2025
+ * @brief Utilities sources
+ */
+
+#include "SVTUtilities/SvtUtilities.h"
