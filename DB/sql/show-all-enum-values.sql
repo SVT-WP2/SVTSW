6378e54bc816cdd59396d7d42554fd60976e51b0
Yasser Corrales <yasser.corrales@gmail.com>
2025-03-31 17:38:46 +0200

    ycm - implementting new DB structure

--- DB/sql/show-all-enum-values.sql
+++
@@ -1,8 +0,0 @@
--- SHOW enum values
-SELECT n.nspname AS enum_schema,  
-       t.typname AS enUm_name,  
-       e.enumlabel AS enum_value
-FROM pg_type t 
-   join pg_enum e on t.oid = e.enumtypid  
-   join pg_catalog.pg_namespace n ON n.oid = t.typnamespace;
-
