a292c12414c197d80d9fb10042de8e471dade91a
Yasser Corrales <yasser.corrales.morales@cern.ch>
2025-04-17 10:56:10 -0600

    ycm update ennum types

--- DB/py/showAllEnumValues.py
+++ DB/py/showAllEnumValues.py
@@ -2,33 +2,56 @@ import svtconfigdbinteractions as configdb
 
 schemas = ["test", "Prod"]
 
-enum_foundry = {"enum_foundry": ["TowerJazz"]}
-enum_waferTech = {"enum_waferTech": ["TPSCo65"]}
-enum_engineeringRun = {"enum_engineeringRun": ["ER1"]}
-enum_waferMapOrientation = {"enum_waferMapOrientation": [
-    "North", "South", "East", "West"
-]}
-enum_asicFamilyType = {"enum_asicFamilyType": ["babyMOSS",
-                                               "MOSS",
-                                               "NKF7",
-                                               "APTS",
-                                               "NKF6",
-                                               "NKF5"]}
-
-enum_types_dict = {**enum_foundry, **enum_waferTech,
-                   **enum_engineeringRun, **enum_waferMapOrientation,
-                   **enum_asicFamilyType}
+types = [
+    {"enum_engineeringRun": ["ER1", "ER2", "ER3", "LAS1", "Ancillary1"]},
+    {"enum_foundry": ["TowerSemiconductor", "Xfab"]},
+    {"enum_waferTech": ["TPSCo65", "Xfab110"]},
+    {
+        "enum_asicFamilyType": [
+            "MOSS",
+            "BabyMOSS",
+            "NKF7",
+            "MOSAIX",
+            "BabyMOSAIX",
+            "LAS",
+            "Ancillary",
+        ]
+    },
+    {"enum_waferMapOrientation": ["North", "South", "East", "West"]},
+    {
+        "wpConnectionType": [
+            "TCPIP",
+            "GPIB",
+            "RS232",
+            "USB",
+            "Ethernet",
+            "Modbus",
+            "LAN",
+        ]
+    },
+    {"wpGeneralLocation"["CERN_186_R_E10", "Prague", "LosAlamos", "BNL", "RAL"]},
+    {"wpSwType": ["Sentio", "VeloxCascade"]},
+    {"wpVendor": ["MPI", "CascadeMicrotech", "FormFactor"]},
+    {"pcVendor": ["MPI", "Korea", "Synergie", "FormFactorPC"]},
+    {"pcName": ["NKF7_MPI", "BabyMOSS_Korea", "Mosaix_Korea"]},
+    {"pcModel": ["NKF7", "MosaixLeft", "MosaixRight", "LAS", "BabyMOSS", "Ancillary"]},
+    {"pcLocation": ["CERN", "Prague", "LosAlamos", "BNL", "RAL"]},
+    {"pcType": ["Vertical", "Cantilever"]},
+    {"contctMechanicalQuality": ["Good", "Bad", "AvoidIfPossible"]},
+    {"waferInMachineStatus": ["Loaded", "Unloaded"]},
+]
 
 for schema in schemas:
-    for type_name, values in enum_types_dict.items():
+    for type_name, values in types.items():
         if len(values):
             for value in values:
-                configdb.addEnumValue(schema, type_name, value)
+                print(f"{schema}, {type_name}, {value}")
+                # configdb.addEnumValue(schema, type_name, value)
 
-configdb.commit()
-
-enum_values = configdb.showAllEnumValues()
-for row in enum_values:
-    print(row)
+# configdb.commit()
+#
+# enum_values = configdb.showAllEnumValues()
+# for row in enum_values:
+#     print(row)
 
 configdb.close()
