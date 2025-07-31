8a25c5445ee3e97aaccdfa72c8f1aaef1bada59a
Yasser Corrales <yasser.corrales@gmail.com>
2025-07-31 11:01:22 +0200

    yc add option to define configuration file at start up

--- DB/svt-db-agent/SvtDbAgent.sh
+++ DB/svt-db-agent/SvtDbAgent.sh
@@ -4,5 +4,11 @@
 
   thisScriptPath="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]:-0}")")" && pwd)"
 
+  set +u
+  if [ -n "$1" ]; then
+    CONF_FILE=${1:-}
+    export "$(grep -E -v "(^#.*|^$)" "$CONF_FILE" | xargs -0)"
+  fi
+  set -u
   "$thisScriptPath"/build/bin/svt_db_agent
 )
