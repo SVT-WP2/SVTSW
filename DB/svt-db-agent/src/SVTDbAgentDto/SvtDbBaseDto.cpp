f6bbceffeb68379260430835cdb4ab8f47d9cd4a
Yasser Corrales <yasser.corrales@gmail.com>
2025-08-14 11:26:29 +0200

    yc add a DTO base class

---
+++ DB/svt-db-agent/src/SVTDbAgentDto/SvtDbBaseDto.cpp
@@ -0,0 +1,303 @@
+/*!
+ * @file SvtDbBaseDto.cpp
+ * @author Y. Corrales <ycorrale@cern.ch>
+ * @date Aug-2025
+ * @brief Base DTO class implementation
+ */
+
+#include "SVTDbAgentDto/SvtDbBaseDto.h"
+#include <stdexcept>
+#include <utility>
+#include <vector>
+#include "SVTDb/SvtDbInterface.h"
+#include "SVTDb/sqlmapi.h"
+#include "SVTDbAgentDto/SvtDbWaferTypeDto.h"
+#include "SVTDbAgentService/SvtDbAgentMessage.h"
+#include "SVTUtilities/SvtLogger.h"
+#include "SVTUtilities/SvtUtilities.h"
+
+//========================================================================+
+bool SvtDbAgent::SvtDbBaseDto::getAllEntriesFromDB(
+    std::vector<SvtDbEntry> &entries, const SvtDbFilters &filters)
+{
+  entries.clear();
+  SimpleQuery query;
+
+  std::string full_tableName =
+      SvtDbAgent::db_schema + std::string(".") + GetTableName();
+  query.setTableName(full_tableName);
+
+  std::vector<std::string> allColumns(GetIntColNames().begin(),
+                                      GetIntColNames().end());
+  allColumns.insert(allColumns.end(), GetStringColNames().begin(),
+                    GetStringColNames().end());
+
+  for (const auto &colName : allColumns)
+  {
+    query.addColumn(colName);
+  }
+
+  for (const auto &filter : filters.intFilters)
+  {
+    if (std::find(GetIntColNames().begin(), GetIntColNames().end(),
+                  filter.first) != GetIntColNames().end())
+    {
+      query.addWhereIn(filter.first, filter.second);
+    }
+    else
+    {
+      {
+        Singleton<SvtLogger>::instance().logError(
+            "Wrong filter: column with name " + filter.first +
+            " does not exists in table " + GetTableName());
+        return false;
+      }
+    }
+  }
+  for (const auto &filter : filters.strFilters)
+  {
+    if (std::find(GetIntColNames().begin(), GetIntColNames().end(),
+                  filter.first) != GetIntColNames().end())
+    {
+      query.addWhereEquals(filter.first, filter.second);
+    }
+    else
+    {
+      {
+        Singleton<SvtLogger>::instance().logError(
+            "Wrong filter: column with name " + filter.first +
+            " does not exists in table " + GetTableName());
+        return false;
+      }
+    }
+  }
+
+  try
+  {
+    vector<vector<MultiBase *>> rows;
+    query.doQuery(rows);
+
+    for (vector<MultiBase *> row : rows)
+    {
+      if (row.size() != allColumns.size())
+      {
+        throw std::range_error("return row size unmatches query list size");
+      }
+      SvtDbEntry entry;
+      for (size_t colNameId{0}; colNameId < allColumns.size(); ++colNameId)
+      {
+        std::string &colName = allColumns.at(colNameId);
+        if (std::find(GetIntColNames().begin(), GetIntColNames().end(),
+                      colName) != GetIntColNames().end())
+        {
+          entry.int_values[colName] =
+              (row.at(colNameId)) ? row.at(colNameId)->getInt() : -1;
+        }
+        else if (std::find(GetStringColNames().begin(),
+                           GetStringColNames().end(),
+                           colName) != GetStringColNames().end())
+        {
+          entry.string_values[colName] =
+              (row.at(colNameId)) ? row.at(colNameId)->getString() : "";
+        }
+        else
+        {
+          throw std::runtime_error("Column name " + colName +
+                                   " was not found in DTO");
+        }
+      }
+      entries.push_back(entry);
+    }
+
+    if (filters.intFilters.find("id") != filters.intFilters.end())
+    {
+      if (filters.intFilters.at("id").size() != entries.size())
+      {
+        throw std::runtime_error(
+            "unmatching returned elements and requested filter size");
+      }
+    }
+  }
+  catch (const std::exception &e)
+  {
+    Singleton<SvtLogger>::instance().logError(e.what());
+    entries.clear();
+    return false;
+  }
+
+  return true;
+}
+
+//========================================================================+
+bool SvtDbAgent::SvtDbBaseDto::getEntryWithId(SvtDbEntry &entry, int id)
+{
+  SvtDbFilters filters;
+  filters.intFilters.insert({"id", {id}});
+  std::vector<SvtDbEntry> entries;
+  if (!getAllEntriesFromDB(entries, filters))
+  {
+    return false;
+  }
+  entry = std::move(entries.at(0));
+  return true;
+}
+
+//========================================================================+
+bool SvtDbAgent::SvtDbBaseDto::createEntryInDB(const SvtDbEntry &entry)
+{
+  SimpleInsert insert;
+
+  std::string tableName = SvtDbAgent::db_schema + "." + GetTableName();
+  insert.setTableName(tableName);
+
+  //! checkinput values and Add columns & values
+  for (const auto &colName : GetIntColNames())
+  {
+    if (colName == "id")
+    {
+      continue;
+    }
+    if (entry.int_values.at(colName) < 0)
+    {
+      return false;
+    }
+    insert.addColumnAndValue(colName, entry.int_values.at(colName));
+  }
+  for (const auto &colName : GetStringColNames())
+  {
+    if (entry.string_values.at(colName).empty())
+    {
+      return false;
+    };
+    insert.addColumnAndValue(colName, entry.string_values.at(colName));
+  }
+
+  if (!insert.doInsert())
+  {
+    rollbackUpdate();
+    return -1;
+  }
+  commitUpdate();
+  return true;
+}
+
+//========================================================================+
+void SvtDbAgent::SvtDbBaseDto::parseFilter(const nlohmann::json &msgData,
+                                           SvtDbFilters &filters)
+{
+  if (msgData.contains("filter"))
+  {
+    const auto filterData = msgData["filter"];
+    if (filterData.contains("ids"))
+    {
+      filters.intFilters.insert(
+          {"id", filterData["ids"].get<std::vector<int>>()});
+    }
+  }
+}
+
+//========================================================================+
+void SvtDbAgent::SvtDbBaseDto::getAllEntriesReplyMsg(
+    const std::vector<SvtDbEntry> &entries, SvtDbAgentReplyMsg &msgReply)
+{
+  try
+  {
+    nlohmann::ordered_json data;
+    nlohmann::ordered_json items = nlohmann::json::array();
+    for (const auto &entry : entries)
+    {
+      nlohmann::ordered_json entry_j;
+      for (const auto &item : entry.int_values)
+      {
+        entry_j[item.first] = item.second;
+      }
+      for (const auto &item : entry.string_values)
+      {
+        entry_j[item.first] = item.second;
+      }
+      items.push_back(entry_j);
+    }
+    data["items"] = items;
+    msgReply.setData(data);
+    msgReply.setStatus(
+        SvtDbAgent::msgStatus[SvtDbAgent::SvtDbAgentMsgStatus::Success]);
+    msgReply.setError(0, "");
+  }
+  catch (const std::exception &e)
+  {
+    throw e;
+    return;
+  }
+}
+
+//========================================================================+
+void SvtDbAgent::SvtDbBaseDto::getAllEntries(const SvtDbAgentMessage &msg,
+                                             SvtDbAgentReplyMsg &replyMsg)
+{
+  const auto &msgData = msg.getPayload()["data"];
+  SvtDbFilters filters;
+  parseFilter(msgData, filters);
+
+  std::vector<SvtDbAgent::SvtDbEntry> entries;
+  if (getAllEntriesFromDB(entries, filters))
+  {
+    getAllEntriesReplyMsg(entries, replyMsg);
+  }
+}
+
+//========================================================================+
+void SvtDbAgent::SvtDbBaseDto::createEntry(const SvtDbAgentMessage &msg,
+                                           SvtDbAgentReplyMsg &replyMsg)
+{
+  const auto &msgData = msg.getPayload()["data"];
+  if (!msgData.contains("create"))
+  {
+    throw std::runtime_error("Object item create was found");
+  }
+
+  auto entry_j = msgData["create"];
+  SvtDbAgent::SvtDbEntry entry;
+
+  parseData(entry_j, entry);
+
+  //! create entry in DB
+  if (!createEntryInDB(entry))
+  {
+    throw std::runtime_error("Wafer type was not created");
+    return;
+  }
+
+  const auto newEntryId = SvtDbInterface::getMaxId(GetTableName());
+  getEntryWithId(entry, newEntryId);
+  createEntryReplyMsg(entry, replyMsg);
+}
+
+//========================================================================+
+void SvtDbAgent::SvtDbBaseDto::createEntryReplyMsg(
+    const SvtDbEntry &entry, SvtDbAgentReplyMsg &msgReply)
+{
+  try
+  {
+    nlohmann::ordered_json data;
+    nlohmann::ordered_json entry_j;
+    for (const auto &item : entry.int_values)
+    {
+      entry_j[item.first] = item.second;
+    }
+    for (const auto &item : entry.string_values)
+    {
+      entry_j[item.first] = item.second;
+    }
+
+    data["entity"] = entry_j;
+    msgReply.setData(data);
+    msgReply.setStatus(
+        SvtDbAgent::msgStatus[SvtDbAgent::SvtDbAgentMsgStatus::Success]);
+    msgReply.setError(0, "");
+  }
+  catch (const std::exception &e)
+  {
+    throw e;
+    return;
+  }
+}
