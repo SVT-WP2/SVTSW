229da0a5fd3e55e8e1ff076a5ad0b89cb3376297
Yasser Corrales <yasser.corralemorales@cern.ch>
2025-09-06 10:46:39 +0200

    yc(db tools) update sql scripts

--- DB/sql/SVT-tables-update.sql
+++ DB/sql/SVT-tables-update.sql
@@ -4,23 +4,6 @@ CREATE SCHEMA IF NOT EXISTS :SchemaName;
 -- SELECT SCHEMA
 SET search_path TO :SchemaName;
 
--- WaferType refereced by:
-ALTER TABLE Wafer ADD FOREIGN KEY (waferTypeId) REFERENCES WaferType (id);
-ALTER TABLE WaferTypeImage ADD FOREIGN KEY (waferTypeId) REFERENCES WaferType (id);
-
--- Wafer referenced by:
-ALTER TABLE WaferLocation ADD FOREIGN KEY (waferId) REFERENCES Wafer (id);
-ALTER TABLE Asic ADD FOREIGN KEY (waferId) REFERENCES Wafer (id);
-
--- ALTER TABLE "Version" ADD FOREIGN KEY ("baseVersion") REFERENCES "Version" ("id");
-
-ALTER TABLE "ProbeCardFamilyType" ADD FOREIGN KEY ("probeCardId") REFERENCES "ProbeCard" ("id");
-
---
--- ALTER TABLE "WaferProbeProject" ADD FOREIGN KEY ("waferTypeId") REFERENCES "WaferType" ("id");
---
--- ALTER TABLE "ProbeCardMaintenance" ADD FOREIGN KEY ("probeCardId") REFERENCES "ProbeCard" ("id");
---
 -- ALTER TABLE "AsicProbing" ADD FOREIGN KEY ("asicId") REFERENCES "Asic" ("id");
 --
 -- ALTER TABLE "WaferLoadedInMachine" ADD FOREIGN KEY ("machineId") REFERENCES "WaferProbeMachine" ("id");
@@ -40,5 +23,28 @@ ALTER TABLE "ProbeCardFamilyType" ADD FOREIGN KEY ("probeCardId") REFERENCES "Pr
 -- ALTER TABLE "WpConfiguration" ADD FOREIGN KEY ("versionId") REFERENCES "Version" ("id");
 --
 -- ALTER TABLE "ProbeCardConfiguration" ADD FOREIGN KEY ("probeCardId") REFERENCES "ProbeCard" ("id");
---
+
 -- ALTER TABLE "ProbeCardConfiguration" ADD FOREIGN KEY ("versionId") REFERENCES "Version" ("id");
+-- DROP TABLE WaferType CASCADE;
+-- DROP TABLE Wafer CASCADE;
+-- DROP TABLE WaferLocation CASCADE;
+-- DROP TABLE WaferTypeImage CASCADE;
+-- DROP TABLE Asic CASCADE;
+
+-- DELETE FROM waferlocation;
+-- DELETE FROM asic;
+-- DELETE FROM wafer;
+-- DELETE FROM waferprobeproject;
+-- DELETE FROM wafertype;
+--
+-- ALTER SEQUENCE asic_id_seq RESTART;
+-- ALTER SEQUENCE wafer_id_seq RESTART;
+-- ALTER SEQUENCE waferprobeproject_id_seq RESTART;
+-- ALTER SEQUENCE wafertype_id_seq RESTART;
+
+-- ALTER TYPE "asicFamilyType" RENAME VALUE 'AOIO' TO 'AO10';
+-- ALTER TYPE "asicFamilyType" RENAME VALUE 'AOIO_P' TO 'AO10P';
+-- ALTER TYPE "asicFamilyType" RENAME VALUE 'AOIO_B' TO 'AO10B';
+-- ALTER TYPE "asicFamilyType" RENAME VALUE 'AFIS' TO 'AF15';
+-- ALTER TYPE "asicFamilyType" RENAME VALUE 'AFISP' TO 'AF15P';
+-- ALTER TYPE "asicFamilyType" RENAME VALUE 'AFISB' TO 'AF15B';
