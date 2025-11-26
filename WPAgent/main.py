e08b0a24231a1d30b655a264519d875a849fcfa9
angie-kostina <anhelina.kostina@cern.ch>
2025-11-26 11:14:25 +0100

    separate info about functions into json and add logic to main

--- WPAgent/main.py
+++ WPAgent/main.py
@@ -1,5 +1,14 @@
 from wafer_prober_agent import WaferProberAgent
 import fire
+import sys
+
+# Special handling for: python main.py send Help MoveChuckXY
+if len(sys.argv) >= 4 and sys.argv[1] == "send" and sys.argv[2] == "help":
+    # Convert: python main.py send Help MoveChuckXY
+    # To:      python main.py send Help --params='{"command":"MoveChuckXY"}'
+    if not sys.argv[3].startswith("--"):
+        command_name = sys.argv[3]
+        sys.argv[3] = f'--params={{"command":"{command_name}"}}'
 
 if __name__ == "__main__":
     fire.Fire(WaferProberAgent)
