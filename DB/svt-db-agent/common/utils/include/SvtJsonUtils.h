b20c2968798d18f512473135aa82c299f0e9a507
Yasser Corrales <yasser.corrales.morales@cern.ch>
2025-11-29 21:29:15 -0700

    [Db-Agent] yc: use const qualifiers for json input

--- DB/svt-db-agent/common/utils/include/SvtJsonUtils.h
+++ DB/svt-db-agent/common/utils/include/SvtJsonUtils.h
@@ -12,15 +12,15 @@ using json = nlohmann::json;
 
 namespace SvtUtils
 {
-  bool readStringVariable(json &config, const std::string &key, std::string &var);
-  bool readIntegerVariable(json &config, const std::string &key, int &var);
-  bool readDoubleVariable(json &config, const std::string &key, double &var);
-  bool readBooleanVariable(json &config, const std::string &key, bool &var);
-  bool readStringVector(json &config, const std::string &key,
+  bool readStringVariable(const json &config, const std::string &key, std::string &var);
+  bool readIntegerVariable(const json &config, const std::string &key, int &var);
+  bool readDoubleVariable(const json &config, const std::string &key, double &var);
+  bool readBooleanVariable(const json &config, const std::string &key, bool &var);
+  bool readStringVector(const json &config, const std::string &key,
                         std::vector<std::string> &var);
-  bool readIntegerVector(json &config, const std::string &key,
+  bool readIntegerVector(const json &config, const std::string &key,
                          std::vector<int> &var);
-  bool readDoubleVector(json &config, const std::string &key,
+  bool readDoubleVector(const json &config, const std::string &key,
                         std::vector<double> &var);
   bool recursive_erase_key(json &j, const std::string_view &key);
 }  // namespace SvtUtils
