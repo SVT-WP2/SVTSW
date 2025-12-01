37a282cff5169684675c5bda24489925fb64068c
Oleksandr Korchak <oleksandr.korchak@gmail.com>
2025-12-01 20:45:25 +0100

    [WIP] Docker :: db-agent

--- DB/svt-db-agent/compile.sh
+++ DB/svt-db-agent/compile.sh
@@ -1,6 +1,8 @@
+#! /usr/bin/env bash
+
 [[ $(uname -s) == 'Darwin' ]] && export PKG_CONFIG_PATH="/opt/homebrew/lib/postgresql@17/pkgconfig:/opt/homebrew/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 mkdir -p build && cd build || exit
-[[ ! ${1} == '-u' ]] &&
-  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../ -B.
+[[ ! ${1} == "-u" ]] &&
+  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S ../ -B .
 make -j
