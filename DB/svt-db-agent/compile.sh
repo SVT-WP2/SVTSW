9585a5ab0b2b6d125e936749ef53d699593097bc
Yasser Corrales <yasser.corrales@gmail.com>
2025-12-01 11:07:30 -0700

    feat[Db-Agent] yc: check submodule initialization

--- DB/svt-db-agent/compile.sh
+++ DB/svt-db-agent/compile.sh
@@ -1,3 +1,4 @@
+git submodule status | grep --quiet '^-' && git submodule update --init
 [[ $(uname -s) == 'Darwin' ]] && export PKG_CONFIG_PATH="/opt/homebrew/lib/postgresql@17/pkgconfig:/opt/homebrew/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 mkdir -p build && cd build || exit
