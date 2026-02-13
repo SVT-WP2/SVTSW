bc67696232e749431c57e6f54b911d1b8b26d579
Yasser Corrales <yasser.corrales@gmail.com>
2026-02-13 10:10:43 -0700

    yc(DB): update compilation commands

--- DB/svt-db-agent/compile.sh
+++ DB/svt-db-agent/compile.sh
@@ -1,7 +1,6 @@
 git submodule status | grep --quiet '^-' && git submodule update --init
 [[ $(uname -s) == 'Darwin' ]] && export PKG_CONFIG_PATH="/opt/homebrew/lib/postgresql@17/pkgconfig:/opt/homebrew/lib/pkgconfig:$PKG_CONFIG_PATH"
 
-mkdir -p build && cd build || exit
 [[ ! ${1} == '-u' ]] &&
-  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../ -B.
-make -j
+  cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build -j"$(nproc)"
