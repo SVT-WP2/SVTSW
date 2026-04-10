f8eca9e12226134d9ee77262e665351a9a805dc1
Yasser Corrales <yasser.corrales@gmail.com>
2026-04-10 17:56:33 -0600

    [Db-agent] yc::chore move PKG_CONFIG_PATH definition to cmake

--- DB/svt-db-agent/compile.sh
+++ DB/svt-db-agent/compile.sh
@@ -1,5 +1,3 @@
-[[ $(uname -s) == 'Darwin' ]] && export PKG_CONFIG_PATH="/opt/homebrew/lib/postgresql@17/pkgconfig:/opt/homebrew/lib/pkgconfig:$PKG_CONFIG_PATH"
-
 [[ ! ${1} == '-u' ]] &&
   cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build -j"$(nproc)"
