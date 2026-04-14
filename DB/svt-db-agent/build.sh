3af2c784c8b7c867b0844d044cf5beb910eff7f7
Yasser Corrales <yasser.corrales@gmail.com>
2026-04-14 07:09:48 -0600

    yc::chore(agent::kafka) add multi-stages build and deploy

--- DB/svt-db-agent/build.sh
+++ DB/svt-db-agent/build.sh
@@ -6,4 +6,4 @@ thisScriptPath=$(cd "$(dirname "${BASH_SOURCE[0]:-0}")" &>/dev/null && pwd -P)
 
 # update git_tag before build the docker image
 "${thisScriptPath}"/compile.sh -u -n
-docker build -t localhost/svt.db-agent:latest .
+docker build --target db-agent-build -t localhost/svt.db-agent-build:latest .
