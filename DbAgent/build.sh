93fa2f74098e02149e37dfd6b11d99500e9fbc1a
Yasser Corrales <yasser.corrales@gmail.com>
2026-04-14 19:18:33 -0600

    yc::feat(docker) change db-agent image tag

--- DbAgent/build.sh
+++ DbAgent/build.sh
@@ -6,4 +6,4 @@ THIS_SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]:-0}")" &>/dev/null && pwd -P)
 
 # update git_tag before build the docker image
 "${THIS_SCRIPT_PATH}"/app/compile.sh -u -n
-docker build --target db-agent-deploy -t localhost/svt.db-agent-deploy:latest .
+docker build --target db-agent-deploy -t ycorrales/svt.db-agent.deploy:latest .
