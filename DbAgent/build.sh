22c00fae6b50f81d0af4dbfbb6aad9eb7a7498b1
Yasser Corrales <yasser.corrales@gmail.com>
2026-04-20 10:35:52 -0600

    yc::chore update build script

--- DbAgent/build.sh
+++ DbAgent/build.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 set -euo pipefail
 
-#get this script path
+# get this script path
 THIS_SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]:-0}")" &>/dev/null && pwd -P)
 
 # update git_tag before build the docker image
 "${THIS_SCRIPT_PATH}"/app/compile.sh -u -n
-# docker build --target db-agent-deploy -t ycorrales/svt.db-agent.deploy:latest .
-docker compose build "$@"
+
+if [[ -n "${1:-}" ]]; then
+  docker compose build "$@"
+else
+  docker build -f Dockerfile-dbagent --target db-agent-deploy -t ycorrales/svt.db-agent.deploy:latest .
+fi
